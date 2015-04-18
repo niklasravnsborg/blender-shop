915b4badf270a1f11c5b3c9fc42bc29c1847900c
Niklas Ravnsborg-Gjertsen <niklasravnsborg@gmail.com>
2015-04-18 23:50:27 +0200

    add hero section with backend settings

--- src/settings.php
+++ src/settings.php
@@ -31,3 +31,37 @@ add_action('admin_menu', function() use($page_slug, $title) {
 		}
 	);
 });
+
+add_action('admin_init', function() use($page_slug) {
+	// Register settings so that POST is automatically handled
+	register_setting($page_slug, 'hero_headline' );
+	register_setting($page_slug, 'hero_description' );
+
+	// Add new section to theme-settings page
+	add_settings_section(
+		'landing_page',
+		'Landing Page',
+		function() { echo '<p>Customize the Landing Page.</p>'; },
+		$page_slug
+	);
+
+	// Add a field to use in our landing_page section
+	add_settings_field(
+		'hero_headline', // $id
+		'Hero Headline', // $title
+		'text_input', // $callback
+		$page_slug, // $page
+		'landing_page', // $section
+		['name' => 'hero_headline'] // $args (array for $callback)
+	);
+
+	add_settings_field(
+		'hero_description',
+		'Hero Description',
+		'text_input',
+		$page_slug,
+		'landing_page',
+		['name' => 'hero_description']
+	);
+
+});
