630bbe52485eb66151ca56b1556d1011c3404f24
Niklas Ravnsborg-Gjertsen <niklasravnsborg@gmail.com>
2015-04-21 20:51:12 +0200

    add wordpress header menu

--- src/functions.php
+++ src/functions.php
@@ -10,6 +10,21 @@ if (! class_exists('Timber')) {
 // remove the admin bar from the front end
 add_filter('show_admin_bar', '__return_false');
 
+
+
+// add header_menu to Wordpress
+add_action('init', function() {
+	register_nav_menu('header_menu', __( 'Header Menu' ));
+});
+
+// add header_menu to Timber Context
+add_filter('timber_context', function($data) {
+	$data['header_menu'] = new TimberMenu('header_menu');
+	return $data;
+});
+
+
+
 // add footer_menu to Wordpress
 add_action('init', function() {
 	register_nav_menu('footer_menu', __( 'Footer Menu' ));
@@ -20,6 +35,8 @@ add_filter('timber_context', function($data) {
 	$data['footer_menu'] = new TimberMenu('footer_menu');
 	return $data;
 });
+
+
 
 // Settings Page
 include 'settings.php';
