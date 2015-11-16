896fad3e25fd8f688c25a6bbac186b762e7dae61
Niklas Ravnsborg-Gjertsen <niklasravnsborg@gmail.com>
2015-11-16 18:10:13 +0100

    add quantity counter for cart

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,15 @@
 setTimeout(function() {
 	$('.notice').fadeOut();
 }, 3000);
+
+function counterCount() {
+	var that = $(this);
+	that.parent().find('.counter__number').val(function(index, value) {
+		var value = parseInt(value);
+		that.hasClass('counter__subtract') ? value-- : value++;
+		return value
+	});
+}
+
+$('.counter__subtract').click(counterCount);
+$('.counter__add').click(counterCount);
--- src/singular.php
+++ src/singular.php
@@ -11,24 +11,23 @@ if (is_cart()) {
 	foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
 		$_product   = apply_filters('woocommerce_cart_item_product',    $cart_item['data'],       $cart_item, $cart_item_key);
 		$product_id = apply_filters('woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key);
+		$quantity = false;
 
-		if ($_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key)) {
-			if ($_product->is_sold_individually()) {
-				$product_quantity = sprintf('1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key);
-			} else {
-				$product_quantity = woocommerce_quantity_input(array(
-					'input_name'  => "cart[{$cart_item_key}][qty]",
-					'input_value' => $cart_item['quantity'],
-					'max_value'   => $_product->backorders_allowed() ? '' : $_product->get_stock_quantity(),
-					'min_value'   => '0'
-				), $_product, false);
+		if ($_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters('woocommerce_cart_item_visible', true, $cart_item, $cart_item_key)) {
+			if (!$_product->is_sold_individually()) {
+				$quantity = [
+					'name'  => "cart[{$cart_item_key}][qty]",
+					'value' => $cart_item['quantity'],
+					'min'   => '0',
+					'max'   => $_product->backorders_allowed() ? '' : $_product->get_stock_quantity()
+				];
 			}
 		}
 
 		$cart_products[] = [
 			'title'     => apply_filters('woocommerce_cart_item_name', $_product->get_title(), $cart_item, $cart_item_key),
 			'price'     => apply_filters('woocommerce_cart_item_price', WC()->cart->get_product_price($_product), $cart_item, $cart_item_key),
-			'quantity'  => apply_filters('woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item),
+			'quantity'  => $quantity,
 			'subtotal'  => apply_filters('woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal($_product, $cart_item['quantity']), $cart_item, $cart_item_key)
 		];
 	}
