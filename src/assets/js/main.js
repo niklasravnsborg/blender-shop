071ca0174ec1bcf6b86f00741d96490368d1f193
Niklas Ravnsborg-Gjertsen <niklasravnsborg@gmail.com>
2015-11-22 19:35:39 +0100

    add mobile navigation

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,6 +2,11 @@ setTimeout(function() {
 	$('.notice').fadeOut();
 }, 3000);
 
+$('.header__nav').addClass('header__nav--hidden');
+$('.header__nav__toggle').click(function() {
+	$('.header__nav').toggleClass('header__nav--hidden');
+});
+
 function counterCount() {
 	var that = $(this);
 	that.parent().find('.counter__number').val(function(index, value) {
